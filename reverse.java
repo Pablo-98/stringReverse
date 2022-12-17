d0f3c772365a578b0449ef64f215dc4ee4228a12
Pablo Hernandez <hpablo575@gmail.com>
2022-12-17 12:27:30 -0700

    converted string input into character array

--- reverse.java
+++ reverse.java
@@ -6,13 +6,21 @@
 
  public class reverse {
     public static void main( String [] args) {
+        //declare a str varable
         String str;
+        // added a scanner into program
         Scanner scan = new Scanner(System.in);
-
+        //prompt for user
         System.out.println("type in a word");
+        // scans for user input
         str = scan.nextLine();
         
-        char [] ch = str. toCharArray();
+        // time to convert the string to a character (char) array ([])
+
+        // toCharArray method converts a string to a character array where its contents are initialized with the characters of the string
+        char [] ch = str.toCharArray();
+
+        System.out.println("reverse of a string is: ");
 
 
 
