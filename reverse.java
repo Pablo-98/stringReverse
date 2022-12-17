78b09b6eb812f03bd6045d4d7c802f7eff45a3a8
Pablo Hernandez <hpablo575@gmail.com>
2022-12-17 12:21:31 -0700

    added prompt and a scanner for user input

---
+++ reverse.java
@@ -0,0 +1,20 @@
+/*
+ * program should be able to take a string from user and reverse it 
+ */
+
+ import java.util.Scanner;
+
+ public class reverse {
+    public static void main( String [] args) {
+        String str;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("type in a word");
+        str = scan.nextLine();
+        
+        char [] ch = str. toCharArray();
+
+
+
+    }
+ }
